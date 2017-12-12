99ef5467ed856777eb972abb674c6b5109e4a9e8
Дмитрий <k.dima098@yandex.ru>
2017-12-12 20:33:23 +0300

    Выполнен lesson7\task1, кроме toString()

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,51 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
-
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0) throw IllegalArgumentException()
+    return MatrixImpl<E>(height, width, e)
+}
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E> (override val height: Int, override val width: Int, e: E) : Matrix<E> {
 
-    override val width: Int = TODO()
+    private val map = mutableMapOf<Cell, E>()
 
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
-
-    override fun set(row: Int, column: Int, value: E) {
-        TODO()
+    init {
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                map[Cell(i, j)] = e
+            }
+        }
     }
+
+    override fun get(row: Int, column: Int): E  = get(Cell(row, column))
+
+    override fun get(cell: Cell): E  = map[cell]!!
+
+    override fun set(row: Int, column: Int, value: E) = set(Cell(row, column), value)
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        map[cell] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+            other is MatrixImpl<*> &&
+                    height == other.height &&
+                    width == other.width
 
     override fun toString(): String = TODO()
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + map.hashCode()
+        result = 31 * result + height
+        result = 31 * result + width
+        return result
+    }
 }
 
